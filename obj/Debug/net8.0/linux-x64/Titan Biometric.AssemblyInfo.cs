194dffc24a3e1eb063b891fd1b202f53a8755026
Abhishek Paneri <abhishek.p@bluesurf.co.in>
2024-11-09 15:32:03 +0530

    docker-compose new file

--- obj/Debug/net8.0/linux-x64/Titan Biometric.AssemblyInfo.cs
+++ obj/Debug/net8.0/linux-x64/Titan Biometric.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Titan Biometric")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ce94d4f1fea354fadf083dc96cdb7c8b346619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22cdceeb2c18c13a760a3faa4764cd7a585460df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Titan Biometric")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Titan Biometric")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
